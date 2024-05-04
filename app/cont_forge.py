731420862a156f39f1d33795b2069e6f74cfe0f6
Joseph Marchbanks <153705879+JPHMarchB@users.noreply.github.com>
2024-05-04 17:08:54 -0600

    Added function that takes a user input and sends it to a function to generate content, which is then called in the main fucntion and returns a value

--- app/cont_forge.py
+++ app/cont_forge.py
@@ -1,17 +1,42 @@
 from dotenv import load_dotenv
 import os
 from openai import OpenAI
-
+import argparse
 load_dotenv()
 api_key=os.environ.get("OPENAI_API_KEY")
-client = OpenAI(api_key=api_key)
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ], max_tokens=3
-)
+def content_generation(prompt: str):
+    client = OpenAI(api_key=api_key)
 
-print(completion.choices[0].message)
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You're a social media assistant adept at crafting engaging content ideas for professionals and content creators social media posts"},
+        {"role": "user", "content": f"Generate a social media post idea for '{prompt}'"}], 
+    max_tokens=32, 
+    temperature=0.6
+    )
+
+    print(completion.choices[0].message)
+
+
+def main():
+    print("Running processes")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input", 
+        "-i", 
+        type=str, 
+        required=True
+        )
+    args = parser.parse_args()
+    user_input = args.input
+
+    print(f"User input: {user_input}")
+    content_generation(user_input)
+    pass
+
+
+if __name__ == "__main__":
+    main()
