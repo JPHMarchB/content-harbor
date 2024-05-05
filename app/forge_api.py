6dea12f97da937beac969502ba4b7e686650b673
Joseph Marchbanks <153705879+JPHMarchB@users.noreply.github.com>
2024-05-04 20:25:31 -0600

    Added improved request validation for API calls

--- app/forge_api.py
+++ app/forge_api.py
@@ -1,26 +1,32 @@
-from typing import Union
-from fastapi import FastAPI
-from cont_forge import content_generation, keyword_generation, validate_request_length
+from fastapi import FastAPI, HTTPException
+from cont_forge import content_generation, keyword_generation
 
 app = FastAPI()
 
 @app.get("/generate_content")
 async def content_generation_api(prompt: str):
-    validate_request_length(prompt)
+    request_validation(prompt)
     snippet = content_generation(prompt)
     return {"Snippet": snippet}
 
 @app.get("/generate_keywords")
 async def keyword_generation_api(prompt: str):
-    validate_request_length(prompt)
+    request_validation(prompt)
     keywords = keyword_generation(prompt)
     return {"keywords": keywords}
 
 @app.get("/post_idea")
 async def post_generation_api(prompt: str):
-    validate_request_length(prompt)
+    request_validation(prompt)
     concept = content_generation(prompt) 
     keywords = keyword_generation(prompt)
-    return {"Post Concept": concept, "Post Keywords": keywords}
+    return {
+        "Post Concept": concept, 
+        "Post Keywords": keywords
+        }
+
+def request_validation(prompt: str):
+    if len(prompt) > 24:
+        raise HTTPException(status_code=400, detail="Prompt length exceeds maximum allowed length of 24 characters")
 
 # fastapi dev forge_api.py
