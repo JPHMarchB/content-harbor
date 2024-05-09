52c1d58a5ca17a6020b2056fc9e52c9b3dff5c35
Joseph Marchbanks <153705879+JPHMarchB@users.noreply.github.com>
2024-05-08 18:53:38 -0600

    Established proper connects to AWS Lambda, fixed dependencies, added API calls through AWS function

--- app/forge_api.py
+++ app/forge_api.py
@@ -9,20 +9,20 @@ handler = Mangum(app)
 async def content_generation_api(prompt: str):
     request_validation(prompt)
     snippet = content_generation(prompt)
-    return {"Snippet": snippet, "keywords": []}
+    return {"Snippet":snippet,"keywords":[]}
 
 @app.get("/generate_keywords")
 async def keyword_generation_api(prompt: str):
     request_validation(prompt)
     keywords = keyword_generation(prompt)
-    return {"snippet": None, "keywords": keywords}
+    return {"snippet":None,"keywords":keywords}
 
 @app.get("/post_idea")
 async def post_generation_api(prompt: str):
     request_validation(prompt)
     concept = content_generation(prompt) 
     keywords = keyword_generation(prompt)
-    return {"Post Concept": concept, "Post Keywords": keywords}
+    return {"Post Concept":concept,"Post Keywords":keywords}
 
 def request_validation(prompt: str):
     if len(prompt) > 24:
